2e5519bd51ae04645e34334e1f29893a3d0c0999
henrybowers <hbowers@gmail.com>
2014-11-15 08:52:35 -0500

    quiz2

--- quiz2.R
+++ quiz2.R
@@ -4,44 +4,14 @@ setwd('~/Projects/StatisticalInferencing/StatisticalInference')
 
 #Q1 
 
-atleastone <- 0.17
-both <- 0.06
-father <- 0.12
-
-# P(mother or father) = P(mother) + P(father) - P(mother and father)
-# P(mother)= P(mother or father) - P(father) + P(mother and father)
-mother <- atleastone - father + both
-
 #Q2
-# 75th percentile of uniform PDF 1x1 box
-l <- 0.75
-w <- 1
-a75th <- l*w
 
 #Q3
 
-# P(heads) = p
-# P(tails) = 1-p
-
-# 0 = p*X - (1-p)*Y
-# p*X = (1-p)*Y
-# p*X/Y= (1-p)
-# X/Y = (1-p)/p
-
-
 #Q4
 
-#median of PDF symmetric about 0 = 0
-
 #Q5
+# flip a fair coin 5 times. P(4 or 5 heads)
 
-x <- 1:4
-p <- x/sum(x)
-temp <- rbind(x, p)
-rownames(temp) <- c("X", "Prob")
-temp
-mean <- sum(x*p)
-
-
-#Q6
-
+pheads <- 0.5
+p4or5heads <- pheads^4 + 0.5
